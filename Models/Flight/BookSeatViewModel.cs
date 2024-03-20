e6b10b2d198ed75061f12b9934c309f3395f16c8
Dayana Borisova <04didi03@gmail.com>
2024-03-20 02:50:39 +0200

    Add BookSeat functionality

---
+++ Models/Flight/BookSeatViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AirlineSystemApp.Models.Flight
+{
+    public class BookSeatViewModel
+    {
+        public int FlightId { get; set; }
+        public int PassengerId { get; set; }
+    }
+}
